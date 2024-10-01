bd627c89b3f0eb3c5f72e710faa9db518f437982
김문희 <homoonshi@gmail.com>
2024-10-01 21:03:31 +0900

    add about, register page background image

--- client/src/pages/user/Register.jsx
+++ client/src/pages/user/Register.jsx
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import HeaderNavbar from '../../components/navbar/HeaderNavbar';
+import LoginBackImg from '../../assets/backImg/loginBackImg.png';
 
 const RegisterDiv = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: url(${LoginBackImg});
+  background-size: cover; /* 이미지가 전체를 덮도록 */
+  background-repeat: no-repeat; /* 이미지가 반복되지 않도록 */
 
 `;
 
@@ -9,7 +16,7 @@ const Register = () => {
 
     return(
         <RegisterDiv>
-
+            <HeaderNavbar/>
         </RegisterDiv>
     )
 
