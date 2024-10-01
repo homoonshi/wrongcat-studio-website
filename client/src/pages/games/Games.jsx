6828cd6add0300a2d5da1099f11761bf6c768372
김문희 <homoonshi@gmail.com>
2024-10-01 21:03:09 +0900

    add gamepage background img

--- client/src/pages/games/Games.jsx
+++ client/src/pages/games/Games.jsx
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import HeaderNavbar from '../../components/navbar/HeaderNavbar';
+import GameBackImg from '../../assets/backImg/gameBackImg.png';
 
 const GameDiv = styled.div`
-
+  width: 100vw;
+  height: 100vh;
+  background-image: url(${GameBackImg});
+  background-size: cover; /* 이미지가 전체를 덮도록 */
+  background-position: center; /* 이미지가 가운데에 위치하도록 */
+  background-repeat: no-repeat; /* 이미지가 반복되지 않도록 */
 `;
 
 const Games = () => {
 
     return(
         <GameDiv>
-
+            <HeaderNavbar/>
         </GameDiv>
     )
 
